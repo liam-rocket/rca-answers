e15dcd6268011e0466e4186419aa18310e2ef9ac
Liam Leung <liam.leung@binery.co>
2022-02-08 22:04:09 +0800

    feat: more comfortable module3/ice3

--- module3/ice2/index.js
+++ module3/ice2/index.js
@@ -5,13 +5,77 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+// * BASE
+
 const getSightings = (request, response) => {
   read('data.json', (err, jsonContentObj) => {
     // Obtain data to inject into EJS template
-    const data = jsonContentObj.sightings;
+    let data = jsonContentObj.sightings;
+    let sortBy = 'Default';
+
+    // * MORE COMFORTABLE
+
+    const availableQueries = [
+      'Year',
+      'Season',
+      'Month',
+      'Date',
+      'State',
+      'Country',
+      'Location Details',
+      'Nearest Town',
+      'Nearest Road',
+      'Also Noticed',
+      'Other Witness',
+      'Other Stories',
+      'Time and Conditions',
+      'Environment',
+      'Report Number',
+      'Report Class',
+    ];
+
+    if (Object.keys(request.query).length > 0) {
+      sortBy = request.query.sortBy;
+      let sortKey = sortBy.toUpperCase().replace('_', '_');
+      data = data.map((row) => row[sortKey]).sort((a, b) => b - a);
+    }
 
     // Return HTML to client, merging "index" template with supplied data.
-    response.render('sightings', { sightings: data });
+    response.render('sightings', {
+      sightings: data,
+      queries: availableQueries,
+      sortBy,
+    });
+  });
+};
+
+// * COMFORTABLE
+
+const getYears = (request, response) => {
+  read('data.json', (err, jsonContentObj) => {
+    const sightings = jsonContentObj.sightings;
+    const years = [];
+
+    const filteredYears = sightings.filter((row) => row.YEAR);
+    const sortedYears = filteredYears
+      .map((row) => row.YEAR)
+      .sort((a, b) => b - a);
+
+    sortedYears.forEach((year) => {
+      if (!years.includes(year)) years.push(year);
+    });
+
+    response.render('years', { years });
+  });
+};
+
+const getYearSighting = (request, response) => {
+  read('data.json', (err, jsonContentObj) => {
+    const allSightings = jsonContentObj.sightings;
+    const year = request.params.year;
+
+    const yearSightings = allSightings.filter((row) => row.YEAR === year);
+    response.render('sightings', { sightings: yearSightings });
   });
 };
 
@@ -32,7 +96,9 @@ const getSighting = (request, response) => {
   });
 };
 
-app.get('/sightings', getSightings);
+app.get('/', getSightings);
+app.get('/years', getYears);
+app.get('/year-sighting/:year', getYearSighting);
 app.get('/sightings/:index', getSighting);
 
 app.listen(3004);
