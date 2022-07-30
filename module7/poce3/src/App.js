44414830faa57013e4e69d6754a9e2f7e59e1d82
Liam Leung <liam.leung@binery.co>
2022-07-30 11:42:23 +0800

    feat: 7poce3

---
+++ module7/poce3/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { ReadLater } from './componets/templates';
+
+function App() {
+  return (
+    <div className="App">
+      <ReadLater />
+    </div>
+  );
+}
+
+export default App;
